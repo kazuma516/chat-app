afb86fa47d897fc07f318c2f2de00b2d146c4424
kazuma516 <kizuma.9516@gmail.com>
2022-09-16 21:37:40 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,11 @@
 class MessagesController < ApplicationController
+  
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
   end
+end
 
   def create
     @room = Room.find(params[:room_id])
@@ -21,4 +23,3 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
-end
